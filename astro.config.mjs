0812ac7a537633e6f8754828958f98cf9cf613c3
Optimization Expert <optimizationteamonline@gmail.com>
2026-08-04 23:47:38 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://optimizationexpert.github.io",
   output: "static",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
