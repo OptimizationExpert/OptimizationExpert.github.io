1a9197f414548e2c9c910d0ae474af89098748ba
Optimization Expert <optimizationteamonline@gmail.com>
2026-08-05 06:18:08 +0100

    Update redirects and add trailingSlash option

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://optimizationexpert.github.io",
   output: "static",
+  trailingSlash: "always",
 
   redirects: {
     "/posts/2026/06/20/Advanced-Power-System-Course":
-      "/courses/advanced-power-system",
+      "/advanced-power-system-course/",
+
+    "/posts/2026/06/24/vrp-python-course":
+      "/vehicle-routing-python-course/",
   },
 
   vite: {
