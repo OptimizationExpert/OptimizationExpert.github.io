db39e25191ad85f53eefc2af0a3a4c2c56b7dd3a
Optimization Expert <optimizationteamonline@gmail.com>
2026-08-05 06:13:30 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ export default defineConfig({
   site: "https://optimizationexpert.github.io",
   output: "static",
 
+  redirects: {
+    "/posts/2026/06/20/Advanced-Power-System-Course":
+      "/courses/advanced-power-system",
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
