1a6d6a9da7bac99a2d08ced5e9fe96b5fc039a92
Optimization Expert <optimizationteamonline@gmail.com>
2026-08-06 10:37:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: "https://optexpert.org",
   output: "static",
   trailingSlash: "always",
-
+  
+  export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  
   redirects: {
     "/posts/2026/06/20/Advanced-Power-System-Course":
       "https://optexpert.org/courses/advanced-power-system/",
